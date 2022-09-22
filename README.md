1fcb42b37f9a994058be523da7447b91c444bb90
daidh1 <daidh@goline.vn>
2022-09-22 23:21:54 +0700

    1.0.19

--- README.md
+++ README.md
@@ -7,7 +7,9 @@ This demo [stackblitz](https://stackblitz.com/edit/angular-ngx-scanner-qrcode?fi
 ![Logo](https://raw.githubusercontent.com/id1945/ngx-scanner-qrcode/master/ngx-scanner-qrcode.png)
 
 ## Installation
+
 Install `ngx-scanner-qrcode` from `npm`:
+
 ```bash
 npm install ngx-scanner-qrcode --save
 ```
@@ -25,8 +27,8 @@ import { NgxScannerQrcodeModule } from 'ngx-scanner-qrcode';
 In the Component:
 
 ```html
-<ngx-scanner-qrcode #action="scanner" (data)="output = $event"></ngx-scanner-qrcode>
-<span>{{output}}</span>
+<ngx-scanner-qrcode #action="scanner"></ngx-scanner-qrcode>
+<span>{{action.data}}</span>
 <button (click)="action.toggleCamera()" [disabled]="action.isLoading">{{action.isStart ? 'Stop' : 'Start'}}</button>
 ```
 
@@ -43,7 +45,7 @@ In the Component:
 
 | Field | Description | Type | Default |
 | --- | --- | --- | --- |
-| data | data of qrcode | string | - |
+| event | data of qrcode | string | - |
 | error | error | any | - |
 
 #### Component export
@@ -53,15 +55,45 @@ In the Component:
 | toggleCamera | Active/Inactive camera | function | - |
 | start | Active camera | function | - |
 | stop | Inactive camera | function | - |
+| play | Play function | void | - |
+| pause | Pause function | void | - |
+| download | Download function | string | - |
 | isLoading | Check start fn | boolean | false |
 | isStart | Start Video | boolean | false |
 
+
 #### Support versions
-    1.1.0 >= Angular 9
-    1.0.17 >= Angular 8
+  
+<table>
+  <tr>
+    <th colspan="2">Support versions</th>
+  </tr>
+  <tr>
+    <td>Angular 9</td>
+    <td>1.1.0</td>
+  </tr>
+  <tr>
+    <td>Angular 8</td>
+    <td>1.0.17</td>
+  </tr>
+</table>
 
-Author: `DaiDH`, Tel: `0845882882`
+#### Author Information
+  
+<table>
+  <tr>
+    <th colspan="2">Author Information</th>
+  </tr>
+  <tr>
+    <td>Author</td>
+    <td>DaiDH</td>
+  </tr>
+  <tr>
+    <td>Phone</td>
+    <td>+84845882882</td>
+  </tr>
+</table>
 
-#### License
+![Vietnam](https://raw.githubusercontent.com/id1945/id1945/master/vietnam.gif)
 
 [MIT License](https://github.com/id1945/ngx-scanner-qrcode/blob/master/LICENSE). Copyright (c) 2021 DaiDH
--- projects/ngx-scanner-qrcode/src/lib/ngx-scanner-qrcode.component.ts
+++ projects/ngx-scanner-qrcode/src/lib/ngx-scanner-qrcode.component.ts
@@ -1,28 +1,49 @@
-import { Component, ElementRef, EventEmitter, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import jsQR from './qrcode';
 
 @Component({
   selector: 'ngx-scanner-qrcode',
-  template: `<canvas #canvas [style.width.%]="100" [style.height.%]="100"></canvas>`,
-  queries: { canvas: new ViewChild('divRef', { static: true }) },
-  inputs: ['line', 'color'],
+  template: `<canvas #canvas [style.width.%]="100" [style.height.%]="100"></canvas><video #video playsinline style="display: none"></video><ng-content></ng-content>`,
+  queries: {
+    video: new ViewChild('video', { static: true }),
+    canvas: new ViewChild('canvas', { static: true })
+  },
+  inputs: ['frameConfig', 'textConfig'],
+  host: { 'class': 'ngx-scanner-qrcode' },
   outputs: ['data', 'error'],
   exportAs: 'scanner',
 })
-export class NgxScannerQrcodeComponent {
+export class NgxScannerQrcodeComponent implements OnInit, OnDestroy {
 
-  // private
-  private line = 4;
-  private color = '#008000';
-  private canvas: ElementRef;
-  private data = new EventEmitter<string>();
-  private error = new EventEmitter<any>();
-  private videoElement: HTMLVideoElement | any;
-  private medias: MediaStreamConstraints = { video: { facingMode: "environment" } };
+  // @ViewChild
+  // @Export
+  public video!: ElementRef;
+  public canvas!: ElementRef;
 
-  // public
-  public isLoading = false;
+  // @Output
+  // @Export
+  public error = new EventEmitter<any>();
+  public event = new EventEmitter<string>();
+
+  // @Export
+  public medias: MediaStreamConstraints = { video: { facingMode: "environment" } };
   public isStart = false;
+  public isLoading = false;
+  public data: string;
+
+  /**
+   * Properties
+   * CanvasRenderingContext2D
+   * https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
+   */
+  public frameConfig: Object = {
+    lineWidth: 4,
+    strokeStyle: '#008000'
+  };
+  public textConfig: Object = {
+    font: '30px serif',
+    fillStyle: '#008000'
+  };
 
   ngOnInit(): void {
     this.initBackgroundColor();
@@ -33,7 +54,7 @@ export class NgxScannerQrcodeComponent {
    */
   private initBackgroundColor() {
     const ctx = this.canvas.nativeElement.getContext('2d') as CanvasRenderingContext2D;
-    ctx.fillStyle = "#000000";
+    ctx.fillStyle = '#000000';
     ctx.fillRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
   }
 
@@ -41,24 +62,18 @@ export class NgxScannerQrcodeComponent {
    * toggleCamera
    */
   public toggleCamera() {
-    if (this.isStart) {
-      this.stop()
-    } else {
-      this.start();
-    }
+    if (this.isStart) this.stop();
+    else this.start();
   }
 
   /**
    * start
    * @returns 
    */
-  public start() {
-    if (this.isStart) {
-      return;
-    }
+  public start(): void {
+    if (this.isStart) return;
 
     this.isLoading = true;
-    this.videoElement = document.createElement('video');
 
     /**
      * MediaStream
@@ -66,9 +81,9 @@ export class NgxScannerQrcodeComponent {
      */
     navigator.mediaDevices.getUserMedia(this.medias).then((stream: MediaStream) => {
       this.isStart = true;
-      this.videoElement.srcObject = stream;
-      this.videoElement.setAttribute("playsinline", 'true'); // required to tell iOS safari we don't want fullscreen
-      this.videoElement.play();
+      this.video.nativeElement.srcObject = stream;
+      this.video.nativeElement.setAttribute("playsinline", 'true'); // required to tell iOS safari we don't want fullscreen
+      this.video.nativeElement.play();
       requestAnimationFrame(scanner);
     }).catch(() => this.stop());
 
@@ -80,19 +95,31 @@ export class NgxScannerQrcodeComponent {
       ctx.beginPath();
       ctx.moveTo(begin.x, begin.y);
       ctx.lineTo(end.x, end.y);
-      ctx.lineWidth = this.line;
-      ctx.strokeStyle = this.color;
+      for (let key in this.frameConfig) {
+        ctx[key] = this.frameConfig[key];
+      }
       ctx.stroke();
+    }
+
+    const drawData = (code) => {
+      if (this.textConfig) {
+        for (let key in this.textConfig) {
+          ctx[key] = this.textConfig[key];
+        }
+        const textString = code.data;
+        const metrics = ctx.measureText(textString);
+        ctx.fillText(textString, ((code.location.bottomLeftCorner.x + code.location.bottomRightCorner.x) / 2) - (metrics.width / 2), code.location.bottomRightCorner.y + 40);
+      }
     }
 
     /**
      * scanner
      */
     const scanner = () => {
-      if (this.videoElement.readyState === this.videoElement.HAVE_ENOUGH_DATA) {
-        this.canvas.nativeElement.height = this.videoElement.videoHeight;
-        this.canvas.nativeElement.width = this.videoElement.videoWidth;
-        ctx.drawImage(this.videoElement, 0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+      if (this.video.nativeElement.readyState === this.video.nativeElement.HAVE_ENOUGH_DATA) {
+        this.canvas.nativeElement.height = this.video.nativeElement.videoHeight;
+        this.canvas.nativeElement.width = this.video.nativeElement.videoWidth;
+        ctx.drawImage(this.video.nativeElement, 0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
         const imageData = ctx.getImageData(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
         const code = jsQR(imageData.data, imageData.width, imageData.height, {
           inversionAttempts: "dontInvert",
@@ -102,25 +129,63 @@ export class NgxScannerQrcodeComponent {
           drawFrame(code.location.topRightCorner, code.location.bottomRightCorner);
           drawFrame(code.location.bottomRightCorner, code.location.bottomLeftCorner);
           drawFrame(code.location.bottomLeftCorner, code.location.topLeftCorner);
-          this.data.emit(code.data ? code.data : '');
+          drawData(code);
+          this.event.emit(code?.data ?? null);
+          this.data = code?.data ?? null;
         }
         this.isLoading = false;
+        this.canvas.nativeElement.onclick = () => this.video.nativeElement.paused ? this.play() : this.pause(); // pause when clicked on screen and resume on next click
       }
       requestAnimationFrame(scanner);
     }
   }
 
   /**
-   * stop
+   * Video play
+   * @return void
    */
-  public stop() {
-    this.data.emit(null);
+  public play(): void {
+    this.video?.nativeElement?.play();
+  }
+
+  /**
+   * Video pause
+   * @return void
+   */
+  public pause(): void {
+    this.video?.nativeElement?.pause();
+  }
+
+  /**
+   * Video stop
+   * @return void
+   */
+  public stop(): void {
     this.isStart = false;
     this.isLoading = false;
+    this.event.emit(null);
     try {
-      this.videoElement && this.videoElement.srcObject.getTracks().forEach(track => track.stop());
+      this.video?.nativeElement?.srcObject?.getTracks()?.forEach((track: any) => track?.stop());
     } catch (error) {
-      this.error.emit('No camera detected!');
+      this.error.emit(error);
     }
+  }
+
+  /**
+   * Download image
+   * @param fileName eg: demo.png
+   * @return string
+   */
+  public download(fileName: string): string {
+    const dataURL = this.canvas?.nativeElement?.toDataURL(`image/${fileName?.split('.')?.slice(-1)?.toString()}`);
+    const link = document.createElement('a');
+    link.download = fileName;
+    link.href = dataURL;
+    link.click();
+    return dataURL;
+  }
+
+  ngOnDestroy(): void {
+    this.pause();
   }
 }
